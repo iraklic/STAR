8cbae2a12d7660359fd5037007fbd987aa39ad7f
Irakli Chakaberia <iraklic@rcas6005.rcf.bnl.gov>
2015-07-14 12:17:22 -0400

    added inverse cumulative distribution

--- iTPC/ALTRO_THRESHOLD/altroPlotMaker.C
+++ iTPC/ALTRO_THRESHOLD/altroPlotMaker.C
@@ -1,4 +1,4 @@
-void nHitsDiff()
+void altroPlotMaker()
 	{
 	TFile * altro3 = new TFile("/gpfs01/star/pwg/iraklic/mySimulations/AuAu/Pico_generation/y2014_Altro_3_Out.rectree.root");
 	TFile * altro4 = new TFile("/gpfs01/star/pwg/iraklic/mySimulations/AuAu/Pico_generation/y2014_Altro_4_Out.rectree.root");
@@ -28,4 +28,35 @@ void nHitsDiff()
 //	c->Close();
 //	altro3->Close();
 //	altro4->Close();
+//	============================================================
+	hAltro3->ComputeIntegral();
+	double * integral3 = hAltro3->GetIntegral();
+	hAltro3->SetContent(integral3);	
+
+	hAltro4->ComputeIntegral();
+	double * integral4 = hAltro4->GetIntegral();
+	hAltro4->SetContent(integral4);	
+
+	TH1F * h23 = hAltro3->Clone("h23");
+	TH1F * h24 = hAltro4->Clone("h24");
+
+	for (int i = 1; i <= hAltro3->GetNbinsX(); i++)
+		{
+		h23->SetBinContent(i, 1);
+		h24->SetBinContent(i, 1);
+		}
+	h23->Add(hAltro3, -1);
+	h24->Add(hAltro4, -1);
+	h23->SetLineColor(2);
+	h23->SetTitle("Inverse Cumulative Distribution");
+	h23->Draw();
+	h24->SetLineColor(4);
+	h24->Draw("same");
+
+
+	TLegend * leg1 = new TLegend(0.1, 0.1, 0.5, 0.3);
+	leg1->AddEntry(h23, "Altro_thr = 3", "l");
+	leg1->AddEntry(h24, "Altro_thr = 4", "l");
+	leg1->Draw();
+
 	}
