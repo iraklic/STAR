1b48f9aec80bf3b2e4896758e584acf23926b516
Irakli Chakaberia <iraklic@rcas6012.rcf.bnl.gov>
2023-05-03 16:52:32 -0400

    sielensing the fit

--- macros/dumpSSCalibParams.C
+++ macros/dumpSSCalibParams.C
@@ -21,7 +21,7 @@ void dumpSSCalibParams(const char * fileName, const char * part = "") {
 	TH2F * dYS_zx = (TH2F *) dYS->Project3D("zx");
 	TH2F * dZS_zx = (TH2F *) dZS->Project3D("zx");
 
-	TF1 * fit = new TF1("f", "gaus", -0.1, 0.1);
+	TF1 * fit = new TF1("f", "gaus", -0.2, 0.2);
 
 	dXS_zx->FitSlicesY(fit); TH1F * dXS_zx_1 = (TH1F *) gDirectory->Get("dXS_zx_1"); printHist(dXS_zx_1, "X", part);
 	dYS_zx->FitSlicesY(fit); TH1F * dYS_zx_1 = (TH1F *) gDirectory->Get("dYS_zx_1"); printHist(dYS_zx_1, "Y", part);
@@ -35,17 +35,17 @@ void dumpSSCalibParams(const char * fileName, const char * part = "") {
 		TH1F * dXS_zx_py = (TH1F *) gDirectory->Get("dXS_zx_py");
 		TH1F * dYS_zx_py = (TH1F *) gDirectory->Get("dYS_zx_py");
 		TH1F * dZS_zx_py = (TH1F *) gDirectory->Get("dZS_zx_py");
-		dXS_zx_py->Fit(fit, "R");
-		dYS_zx_py->Fit(fit, "R");
-		dZS_zx_py->Fit(fit, "R");
+		dXS_zx_py->Fit(fit, "RQ");
+		dYS_zx_py->Fit(fit, "RQ");
+		dZS_zx_py->Fit(fit, "RQ");
 		char nameIt[200];
-		sprintf(nameIt, "/afs/rhic.bnl.gov/star/users/iraklic/WWW/Alignment/dXS_fit_%d.gif", i);
+		sprintf(nameIt, "/afs/rhic.bnl.gov/star/users/iraklic/WWW/Alignment/SuperSector/dXS_fit_%d.gif", i);
 		dXS_zx_py->Draw();
 		c->SaveAs(nameIt);
-		sprintf(nameIt, "/afs/rhic.bnl.gov/star/users/iraklic/WWW/Alignment/dYS_fit_%d.gif", i);
+		sprintf(nameIt, "/afs/rhic.bnl.gov/star/users/iraklic/WWW/Alignment/SuperSector/dYS_fit_%d.gif", i);
 		dYS_zx_py->Draw();
 		c->SaveAs(nameIt);
-		sprintf(nameIt, "/afs/rhic.bnl.gov/star/users/iraklic/WWW/Alignment/dZS_fit_%d.gif", i);
+		sprintf(nameIt, "/afs/rhic.bnl.gov/star/users/iraklic/WWW/Alignment/SuperSector/dZS_fit_%d.gif", i);
 		dZS_zx_py->Draw();
 		c->SaveAs(nameIt);
 	}
